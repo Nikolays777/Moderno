715c75a80821b85650f36a73c4d07b6f4c3d62f4
Nikolays777 <dudchak_m@list.ru>
2020-09-19 01:19:25 +0300

    added product-one

--- app/js/main.js
+++ app/js/main.js
@@ -39,6 +39,15 @@ $(function(){
     $('.header__box').toggleClass('active')
   })
 
+  $('.product-one__tabs .tab').on('click', function(event) {
+    const id = $(this).attr('data-id')
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide()
+    $('.product-one__tabs .tabs').find('.tab').removeClass('active')
+    $(this).addClass('active')
+    $('#' + id).addClass('active-tab').fadeIn()
+    return false
+  })
+
     
   const mixer = mixitup('.products__inner-box')
   
