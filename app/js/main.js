f8b3f126980409e01fb8c51a63fac6f57fb967bd
Nikolays777 <dudchak_m@list.ru>
2020-09-24 04:10:48 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-  
+$(function () {
+
   $(".js-range-slider").ionRangeSlider({
     type: "double",
     min: 0,
@@ -19,28 +19,52 @@ $(function(){
     slidesToScroll: 4,
     dots: true,
     arrows: false,
-    
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
   })
 
-  $('.icon-th-list').on('click', function() {
+  $('.icon-th-list').on('click', function () {
     $('.product__item').addClass('list')
     $('.icon-th-list').addClass('active')
     $('.icon-th-large').removeClass('active')
   })
-  $('.icon-th-large').on('click', function() {
+  $('.icon-th-large').on('click', function () {
     $('.product__item').removeClass('list')
     $('.icon-th-large').addClass('active')
     $('.icon-th-list').removeClass('active')
   })
 
-  $('.menu__btn').on('click', function() {
+  $('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle()
   })
-  $('.header__btn-menu').on('click', function() {
+  $('.header__btn-menu').on('click', function () {
     $('.header__box').toggleClass('active')
   })
 
-  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
+  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function (event) {
     const id = $(this).attr('data-id')
     $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide()
     $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active')
@@ -50,7 +74,7 @@ $(function(){
   })
 
   $('input[type="file"], select').styler();
-    
+
   const mixer = mixitup('.products__inner-box')
-  
+
 });
